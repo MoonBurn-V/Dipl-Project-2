a41543ed82adc0c1c9ede390cde2455bbbebef53
Valery Redkin <valeriy.redkin.05@mail.ru>
2026-06-16 14:34:31 +0300

    fix

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors({
     //origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    origin: "http://localhost",
+    //origin: "http://localhost",
+    origin: "http://135.106.159.188",
     credentials: true
 }))
 app.use(express.json({ limit: '100mb' }))
